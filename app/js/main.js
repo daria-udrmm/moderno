858bf18e8a5f511f0ba36bba28ad5b564d38f47e
daria-udrmm <Darya.Vi@yandex.ru>
2020-09-15 21:00:52 +0300

    slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,20 @@
 $(function(){
+
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true,
+        spacing: "5px"
+      });
+
+    $('.feed__slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        prevArrow: $('.prev'),
+        nextArrow: $('.next')
+    });  
+
     var mixer = mixitup('.shop__items');
+
 
 });
